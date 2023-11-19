c6e6c1b4a55904a5956a09c0cc0d5092c21ea92d
AllanFrank2023 <144777913+AllanFrank2023@users.noreply.github.com>
2023-11-19 14:52:39 +0100

    Update and rename calculator.py to calculatorVersion1.py
    List version 1

--- calculatorVersion1.py
+++ calculatorVersion1.py
@@ -4,6 +4,10 @@ from tkinter import ttk
 from math import sqrt
 import re
 
+
+
+
+
 class Calculator():
     def __init__(self) -> None:
         Gui()
@@ -17,14 +21,27 @@ class Calculator():
 class Gui(Calculator):
     def __init__(self):
         self.root = tk.Tk()
-        self.root.title = "Calculator"
+        self.root.title = ("Calculator")
+        self.handler = Handler()
         self.buildLayout()
         self.style()
 
 
         self.root.mainloop()
 
-        
+    def DisplayFrame(self):
+     #__init__(self, master=None):
+     #   super().__init__(master)
+        self.pack()
+        self.result_list = []
+
+        # Create a label to display the results
+        self.result_label = tk.Label(self, text="Results:")
+        self.result_label.pack()
+
+        # Create a button to trigger the display_text method
+        self.display_button = tk.Button(self, text="Display Text", command=self.display_text)
+        self.display_button.pack()   
 
     def buildLayout(self):
 
@@ -40,7 +57,10 @@ class Gui(Calculator):
         # Frame for numbers and operators
         self.numpad_frame = tk.Frame(self.root)
         self.numpad_frame.pack()
-
+        
+        # Frame for result-historic
+        #################################
+        
         #Numpad
         self.list_buttons = [
             "(", ")", "C", "\u2190",
@@ -72,21 +92,26 @@ class Gui(Calculator):
 
         if len(self.display.cget("text")) == 0:
             newText = 0
+        
         else:
                 
             if self.display.cget("text") == "0":
                 newText = text
+             
             else:
                 newText = self.display.cget("text") + text
+             
                     
         self.display.config(text = newText)
-
-
+     
+    
 
     def events(self, event): #Button events
         newText = ""
         text = event.widget.cget("text")
         self.display.focus_set()
+        
+        
 
         match text:
             case "=":
@@ -95,10 +120,13 @@ class Gui(Calculator):
 
                 newText = self.calculate(new)
                 self.display.config(text = newText)
+                #print("testA",newText, new)
+                self.handler.save_result(newText, new)
+                self.handler.display_results()
 
             case "C":
                 self.display.config(text = "0")
-            
+                
 
             case "\u2190": #Backspace
 
@@ -106,31 +134,37 @@ class Gui(Calculator):
                 self.display.config(text = newText)
                 if len(self.display.cget("text")) == 0:
                     self.display.config(text = "0")
-
+                   
             case "+/-":
                 if self.display.cget("text")[0] != "-":
                     newText = "-" + self.display.cget("text")
+                    
                 else:
                     newText = self.display.cget("text")[1:]
+                    
 
                 self.display.config(text = newText)
                 
             case "\u221A": #Squareroot
                 newText = text + "("
                 self.displayText(newText)
+               
             
             case "x^2": 
                 newText = "^2"
                 self.displayText(newText)
+                
 
             case "x^x":
                 print("SAdasdsdsd")
                 newText = "^"
                 self.displayText(newText)
+                
             
             case _:
                 newText = text
                 self.displayText(newText)
+               
 
     def styleChildren(self, widget, bg_color, text_color):
         widget.configure(bg=bg_color)
@@ -153,21 +187,32 @@ class Gui(Calculator):
         self.numpad_frame.config(background = backgroundColor)
         self.display.config(fg = fontColor)
         
+class Handler():
+       
+    def __init__(self):
+       
+        # Initialize an empty list to store results
+        self.result_list = []
+      
+    def save_result(self, newText, new):
+        # l=self.result_list()
+        # Save the result in the list
+        self.result_list.append((newText, new))
         
+        # Keep only the last 10 results
+        if len(self.result_list) > 20:  # Assuming each result is a pair
+            self.result_list = self.result_list[-20:]
+    def display_results(self):
+        print("Results:")
+        # Display up to the last 10 results
+        for i, result_pair in enumerate(reversed(self.result_list)):
+            # Display results in reverse order (latest calculation first)
+            print(f"{i + 1}. {result_pair[0]} = {result_pair[1]}")
+      
+            
+  
 
-    
 
-
-
-
-    
-
-
-
-    
-
-
-
-
+       
 if __name__ == "__main__":
     Calculator()
